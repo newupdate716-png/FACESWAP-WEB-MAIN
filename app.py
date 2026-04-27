8808b2532b363c634d02cd9c7fcbd71b1f1b1a67
newupdate716-png <newupdate716@gmail.com>
2026-04-27 15:03:26 +0600

    Update via SB-SAKIB

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ html_template = """
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="icon" href="https://files.catbox.moe/gt0b6x.jpg" />
   <link rel="apple-touch-icon" href="https://files.catbox.moe/gt0b6x.jpg" />
-  <title>Face Swap Tool - BY Abbas</title>
+  <title>Face Swap Tool - BY SB-SAKIB </title>
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6690222744270600" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
@@ -756,7 +756,7 @@ html_template = """
         <div class="logo-icon">
           <i class="fas fa-camera-retro"></i>
         </div>
-        <span>AB Face Swap</span>
+        <span>SB Face Swap</span>
       </a>
     </div>
   </header>
